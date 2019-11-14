b168ac1521bb148ae73a89187679b1eba980cebf
Vivek Yadav <parth.hargan@icicilombard.com>
2019-11-14 18:36:00 +0530

    settings

--- lib/setting/settings.dart
+++ lib/setting/settings.dart
@@ -6,6 +6,9 @@ class SettingsUI extends StatefulWidget {
 }
 
 class _SettingsUIState extends State<SettingsUI> {
+  double sliderValue = 1;
+  bool hasSearchByRadius = false;
+  bool hasOrderByAscending = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,63 +16,177 @@ class _SettingsUIState extends State<SettingsUI> {
         title: Text("Settings"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('Search by Radius'),
-              Switch(
-                value: false,
-                onChanged: (v) {},
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Search by Radius',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                Switch(
+                  value: hasSearchByRadius,
+                  onChanged: (v) {
+                    setState(() {
+                      hasSearchByRadius = v;
+                    });
+                  },
+                )
+              ],
+            ),
           ),
-          Row(
-            children: <Widget>[Text('Distance'), Text("50 Km")],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'DISTANCE',
+                  style: TextStyle(
+                      color: Colors.black38,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "50 Km",
+                  style: TextStyle(
+                      color: Colors.black38,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
           ),
-          Row(
-            children: <Widget>[
-              Text('Radius'),
-              Slider(
-                value: 1,
-                onChanged: (v) {},
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24),
+            child: Row(
+              children: <Widget>[
+                Text(
+                  'Radius',
+                  style: TextStyle(
+                    color: Colors.black26,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Slider(
+                    activeColor: Colors.black26,
+                    inactiveColor: Colors.black26,
+                    value: sliderValue,
+                    max: 1,
+                    min: 0,
+                    onChanged: (v) {
+                      setState(() {
+                        sliderValue = v;
+                      });
+                    },
+                  ),
+                )
+              ],
+            ),
           ),
-          Text("Order By"),
-          Row(
-            children: <Widget>[
-              Text("Order By Ascending"),
-              Switch(
-                value: false,
-                onChanged: (v) {},
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Order By",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
-          Text("Select Language"),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Text("English"),
-                  Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Text("English"),
-                ],
-              )
-            ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Order By Ascending"),
+                Switch(
+                  value: hasOrderByAscending,
+                  onChanged: (v) {
+                    setState(() {
+                      hasOrderByAscending = v;
+                    });
+                  },
+                )
+              ],
+            ),
           ),
-          Row(
-            children: <Widget>[
-              Text("Current Location"),
-              Text("Surat"),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              "Select Language",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("English"),
+                    ),
+                    Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+                Divider(),
+                Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("English"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.grey.shade100,
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Current Location",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Surat",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
           ),
         ],
       ),
